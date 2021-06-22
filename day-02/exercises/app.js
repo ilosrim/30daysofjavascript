479e3b233cb1d7c685a489a1a57bd309781ddebc
ilosrim <mirsolikhdev@gmail.com>
2021-06-23 00:24:32 +0500

    End exercises

--- day-02/exercises/app.js
+++ day-02/exercises/app.js
@@ -85,3 +85,34 @@ let randomBtn100 = randomNumber * 100;
 let randomFix1 = Math.floor(randomBtn50);
 let randomFix2 = Math.floor(randomBtn100)
 console.log(randomFix1, randomFix2)
+
+// Level 3
+// 1
+let love = 'Love is the best thing in this world. Some love found their love and some are still looking for their love.'
+
+// a
+console.log(love.match(/\blove\b/gi).length)
+
+// b
+const myCout = love.split(/\blove\b/gi).length-1
+console.log(myCout)
+
+// c
+// let y = love.split(' ')
+// function checkAdult(y) {
+//  return y === 'love'
+// }
+// console.log(y.filter(checkAdult).length)
+
+// 2
+let because = 'You cannot end a sentence with because because because is a conjunction'
+console.log(because.match(/because/gi).length)
+
+// 3
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(sentence.replace(/[^a-zA-Z 0-9.]/g,""))
+
+// 4
+let price = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let sum = price.match(/\d+/g).map(i=>Number(i)).reduce((a, b) => a + b, 0)
+console.log(sum)
