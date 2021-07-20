c2d2635986d72fff905bc9b13584687daf18e27b
ilosrim <mirsolikhdev@gmail.com>
2021-07-20 18:27:24 -0400

    day 12

--- day-12/exercices/app.js
+++ day-12/exercices/app.js
@@ -9,3 +9,12 @@ console.log(yearSalary*12-110000); //$124000
 let points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8'].sort((a,b)=>a-b)
 let c = new Set(points)
 console.log(c);
+
+// Level 2
+let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+
+function tenMostFrequentWords(parametr){
+    return `word: 'love', count: ${parametr.match(/love/gi).length}`
+}
+
+console.log(tenMostFrequentWords(paragraph))
